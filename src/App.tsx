08d50a54818af64f15c1c0a58b819b75255bb534
Javier Ong <javierong9898@gmail.com>
2024-04-06 17:04:34 +0800

    create league page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { AwardsPage } from "./pages/awardsPage";
 import { CommunityPage } from "./pages/communityPage";
 import { JourneyPage } from "./pages/journeyPage";
 import { EcoTrackingPage } from "./pages/ecoTrackingPage";
+import { LeaguePage } from "./pages/leaguePage";
 import { GoalsPage } from "./pages/goalsPage";
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route path="/journey" Component={JourneyPage} />
         <Route path="/ecotracking" Component={EcoTrackingPage} />
         <Route path="/goals" Component={GoalsPage} />
+        <Route path="/league" Component={LeaguePage} />
         <Route Component={ProgressPage} />
       </Routes>
     </Router>
